3c7cd5a58d3736aee4b58aa9797e0934fe2207eb
Dzmitry Misiuk <minsler.by@gmail.com>
2012-06-19 14:30:26 +0300

    Added PatternCaunter class and test class for it

---
+++ src/by/minsler/htp/io/PatternCounter.java
@@ -0,0 +1,37 @@
+package by.minsler.htp.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternCounter {
+
+	private Map<String, Integer> elements = new TreeMap<String, Integer>();
+
+	public Map<String, Integer> count(String str, Pattern pattern) {
+		if (str == null || pattern == null) {
+			// TODO for null variable?
+		}
+		Matcher matcher = pattern.matcher(str);
+		while (matcher.find()) {
+			Integer freq = elements.get(matcher.group());
+			elements.put(matcher.group(), (freq == null) ? 1 : freq + 1);
+		}
+		return elements;
+	}
+
+	public Map<String, Integer> count(BufferedReader br, Pattern pattern)
+			throws IOException {
+		String line = br.readLine();
+		this.count(line, pattern);
+		return elements;
+	}
+
+	public void clearCounter() {
+		elements.clear();
+	}
+
+}
---
+++ src/by/minsler/htp/io/TestPatternCounter.java
@@ -0,0 +1,79 @@
+package by.minsler.htp.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class TestPatternCounter {
+
+	public static void main(String[] args) {
+
+		Pattern patternCharcters = Pattern.compile("[a-zA-Z]"); // characters
+		Pattern patternWords = Pattern.compile("[a-zA-Z]+"); // words
+
+		Pattern pattern = patternWords;
+
+		String str = " this is test line";
+		PatternCounter pc = new PatternCounter();
+		System.out.println(pc.count(str, pattern));
+
+		pc.clearCounter();
+
+		String fileName = null;
+		BufferedReader br = null;
+		Map<String, Integer> words = null;
+
+		BufferedReader sysin = new BufferedReader(new InputStreamReader(
+				System.in));
+
+		try {
+			System.out.println("please enter filename for counting of ...");
+
+			fileName = sysin.readLine();
+			br = new BufferedReader(new FileReader(fileName));
+			words = pc.count(br, pattern);
+		} catch (FileNotFoundException e) {
+			System.out.println(fileName + " not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		if (words != null) {
+			String result = fileName + ".count.txt";
+			BufferedWriter in = null;
+			try {
+				in = new BufferedWriter(new FileWriter(result));
+				for (Map.Entry<String, Integer> en : words.entrySet()) {
+					in.write(en.getKey() + " : " + en.getValue() + '\n');
+				}
+
+			} catch (FileNotFoundException e) {
+				System.out.println(fileName + " not found");
+			} catch (Exception e) {
+				// TODO: handle exception
+			} finally {
+				if (in != null) {
+					try {
+						in.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
